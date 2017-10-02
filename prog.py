106fded90880db02432dcd7b7e188f4f1555aca4
myk7hackon <mykparmar007@gmail.com>
2017-10-02 15:34:23 +0530

    Add files via upload

--- prog.py
+++ prog.py
@@ -23,57 +23,38 @@ while 1:
 			#s=list(all_words.keys())[0:10]
 			#print s
 			#word_features=list(all_words.keys())[:1
-			ham=ham+line[1:]
+			ham=ham+line[2:]
 			#print ham
 #print word_features
 		if line[0]=='1':
 			#k=word_tokenize(line[1:])
 			#all_words=nltk.FreqDist(k)
 			#s=list(all_words.keys())[0:10]
-			spam=spam+line[1:]
+			spam=spam+line[2:]
 k=word_tokenize(ham)
 all_words=nltk.FreqDist(k)
 hm=list(all_words.keys())[0:200]
 #print hm
-k=word_tokenize(spam[1:])
+k=word_tokenize(spam)
 all_words=nltk.FreqDist(k)
 sm=list(all_words.keys())[0:200]
 #print sm
+print sm
+print '\n'
+print hm
 total={}
-totally={}
-for a in hm[:99]:
+for a in hm:
 	total[a]=False
-for a in sm[:99]:
+for a in sm:
 	total[a]=True
-for a in hm[99:]:
-	totally[a]=False
-for a in sm[99:]:
-	totally[a]=True
-#print total
-#import random
-#random.shuffle(total)
-#print total
-#train=total[0:200]
-#test=total[200:]
-#classifier=nltk.NaiveBayesClassifier.train(train)
-#spam=spam+ham
+print total
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(total)
 from sklearn.feature_extraction.text import TfidfTransformer
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
-'''from sklearn.feature_extraction.text import CountVectorizer
-count_vects = CountVectorizer()
-X_train_countss = count_vects.fit_transform(totally)
-tf_transformers = TfidfTransformer(use_idf=False).fit(X_train_countss)
-X_test_tfs = tf_transformer.transform(X_train_countss)'''
-#print X_train_tf.shape
-'''l=[w for w in total.values()]
-train=X_train_tf[0:100]
-test=X_train_tf[100:]
-print total.values()'''
-docs_new = [ham[0]+ham[1], 'OpenGL on the GPU is fast']
+docs_new = ['glass repath', 'diagram']
 X_new_counts = count_vect.transform(docs_new)
 X_new_tfidf = tf_transformer.transform(X_new_counts)
 from sklearn.naive_bayes import MultinomialNB
